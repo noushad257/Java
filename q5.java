4ea71ee67a68b1124fb28ee899b1500be86ce32c
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-06 12:41:21 +0600

    write a java program to check validity of integer
    //write a java program to detect whether a number entered is integer or not

---
+++ q5.java
@@ -0,0 +1,11 @@
+//write a java program to detect whether a number entered is integer or not
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        System.out.println("Enter your no : ");
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+    }
+}
