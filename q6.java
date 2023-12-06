0a8651136eac7fdaa241e81e5bf7bf746e032823
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-06 12:42:12 +0600

    printing data
    print the following data
    
     ID : 101
    Title : Iphone 15
    Price : 1895 euros
    Description : Perfect product with best image quality
    Category : Phone

---
+++ q6.java
@@ -0,0 +1,20 @@
+//print the following data
+/*
+ ID : 101
+Title : Iphone 15
+Price : 1895 euros
+Description : Perfect product with best image quality
+Category : Phone
+*/
+
+import java.util.Scanner;
+
+public class q6{
+    public static void main(String[] args) {
+        System.out.println("ID : 101");
+        System.out.println("Title : Iphone 15");
+        System.out.println("Price : 1895 euros");
+        System.out.println("Description : Perfect product with best image quality");
+        System.out.println("Category : Phone");
+    }
+}
