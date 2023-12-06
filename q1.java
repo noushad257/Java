7b10f715b002056fa57dc3483eecdd3d7d1f8706
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-06 12:37:43 +0600

    //write a program to sum three numbers in  java

---
+++ q1.java
@@ -0,0 +1,14 @@
+//write a program to sum three numbers in  java
+public class q1 {
+    public static void main(String[] args) {
+        //question 1
+
+        int a=30;
+        int b=20;
+        int c=24;
+
+        int sum = a+b+c;
+        System.out.println(sum);
+
+    }
+}
