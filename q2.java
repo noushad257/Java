ff62203c7119f60a33556667b6fe74724ee87e49
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-06 12:38:34 +0600

    //write a java program to calculate cgpa

---
+++ q2.java
@@ -0,0 +1,11 @@
+//write a c program to calculate cgpa using marks of three subjects (out of 100)
+public class q2 {
+    public static void main(String[] args) {
+         float sub1=49;
+         float sub2=50;
+         float sub3=66;
+
+         float cgpa=(sub1+sub2+sub3)/30;
+        System.out.println(cgpa);
+    }
+}
