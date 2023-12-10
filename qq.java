32003279651afade02817e8518f68d5790267875
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-10 13:12:42 +0600

    Quick Quiz on operators

---
+++ qq.java
@@ -0,0 +1,35 @@
+public class qq {
+    public static void main(String[] args) {
+        /*
+        question 1:
+        (x-y)/2
+        question 2:
+        (b^2-4ac)/2a
+        question 3:
+        v^2-u^2
+        question 4:
+        s*t-d
+         */
+        float x=10;
+        float y=5;
+        float k=(x-y)/2;
+        System.out.println(k);
+
+        int a=1;
+        int b=2;
+        int c=3;
+        int l=((b*b)-(4*a*c))/(2*a);
+        System.out.println(l);
+
+        int v=10;
+        int u=10;
+        int m=(v*v)-(u*u);
+        System.out.println(m);
+
+        int s=10;
+        int t=11;
+        int d=9;
+        int n=s*t-d;
+        System.out.println(n);
+    }
+}
