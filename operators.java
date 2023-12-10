abff09103c65875395c7d05022a67ec3147104c9
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-10 12:40:05 +0600

    operators

---
+++ operators.java
@@ -0,0 +1,36 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=4; //arithmetic operator
+        int b=6+a;//arithmetic operator
+        int c=6-a;//arithmetic operator
+        int d=6*a;//arithmetic operator
+        int e=6%a;//arithmetic operator
+        //4.8%1.1-->Returns decimal value
+        int f=11;
+        f+=10;//assignment operator
+        int g=10;
+        g*=3;//assignment operator
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(a==b);//comparison operator
+        System.out.println(a!=c);//comparison operator
+        System.out.println(c==c);//comparison operator
+        System.out.println(a>d);//comparison operator
+        System.out.println(d<c);//comparison operator
+        System.out.println(a>b && a>c);//logical operator
+        System.out.println(a>b || a>c);//logical operator
+        System.out.println(2&3);//bitwise operator
+        //bitwise skipable
+//
+//        10
+//        11
+//      ------
+//        10
+
+
+    }
+}
