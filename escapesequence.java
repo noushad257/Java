a253fdf902f91c04be1c6387c4ec3d8c106ea89f
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-15 11:52:36 +0600

    escapesequence

---
+++ escapesequence.java
@@ -0,0 +1,12 @@
+public class escapesequence {
+    public static void main(String[] args) {
+        System.out.println("I am escape sequence");
+        System.out.println("I am escape sequence \"double qotes");
+        System.out.println("I am escape sequence \\ double qotes");
+        System.out.println("I am escape sequence \n double qotes");
+        System.out.println("I am escape sequence \b double qotes");
+        System.out.println("I am escape sequence \t double qotes");
+
+    }
+    }
+
