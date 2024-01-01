7d0f90780df574f8daa6880113a0b8eef6023392
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-01 21:28:06 +0600

    Add files via upload

---
+++ switchcase3.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+public class switchcase3 {
+    public static void main(String[] args) {
+        String var= "Noushad";
+
+        switch (var){
+            case "Kabir":
+                System.out.println("You are almost adult");
+                break;
+            case "Nuhash":
+                System.out.println("You are going to join a job ");
+                break;
+            case "noushad":
+                System.out.println("You are going to get retair ");
+                break;
+            default:
+                System.out.println("Enjoy your life");
+
+        }
+
+    }
+}
