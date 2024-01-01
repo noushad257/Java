20bd10dd5a4f1fe690568913fbeffa5694a2e7a2
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-01 21:35:30 +0600

    Add files via upload

---
+++ q12.java
@@ -0,0 +1,21 @@
+/*
+  what will be the output of the program
+
+  int a=10;
+  if(a=11)
+      System.out.printlm("I am 11");
+  else
+      System.out.printlm("I am not 11");
+ */
+
+public class q12 {
+    public static void main(String[] args) {
+        int a=10;
+
+        if(a == 11){
+            System.out.println("I am 11");
+        }else{
+            System.out.println("I am not 11");
+        }
+    }
+}
