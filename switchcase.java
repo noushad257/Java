187ab8c40cee9d15be58ab7baffa1e9aaff61727
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-01 21:10:06 +0600

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+public class switchcase {
+    public static void main(String[] args) {
+        int age;
+        System.out.println("Enter your age");
+        Scanner sc=new Scanner(System.in);
+        age= sc.nextInt();
+
+        switch (age){
+            case 18:
+                System.out.println("You are almost adult");
+                break;
+            case 21:
+                System.out.println("You are going to join a job ");
+                break;
+            case 60:
+                System.out.println("You are going to get retair ");
+                break;
+            default:
+                System.out.println("Enjoy your life");
+
+        }
+
+    }
+}
