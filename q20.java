92c199ab75316873e6fff71da33ad102c22bad63
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 03:06:41 +0600

    Add files via upload

---
+++ q20.java
@@ -0,0 +1,9 @@
+//write a program to print multiplication table of a given number n
+public class q20 {
+    public static void main(String[] args) {
+      int n=7;
+      for(int i=1;i<=10;i++){
+          System.out.printf("%d X %d=%d\n ",n,i,n*i);
+      }
+    }
+}
