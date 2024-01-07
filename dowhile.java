39e38c2f47b1707b8517ee12e4cc388dbbdcad6b
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 01:41:21 +0600

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String[] args) {
+//        int a=0 ;
+//        while(a<5){
+//            System.out.println(a);
+//            a++;
+        int b=0;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<5);
+    }
+}
