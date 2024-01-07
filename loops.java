5a469139277aef72eda443b042e3f47047ad18ca
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 01:18:33 +0600

    While loop

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+      //while loop
+        int i = 0;
+      while(i<3){
+          System.out.println(i);
+          i++;
+      }
+      while (true){
+          System.out.println("i am infinite while loop");
+      }  //dont use 1 or 0 java support boolean so use false and true
+    }
+}
