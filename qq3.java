bdf6fefdff832c2c6a06794b522b35b4b1aa7e43
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 01:47:47 +0600

    print number 1 to 100

---
+++ qq3.java
@@ -0,0 +1,12 @@
+
+//print number 1 to 100
+public class qq3 {
+    public static void main(String[] args) {
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<=100);
+    }
+
+}
