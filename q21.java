82515aa531e20e5729c849a5c75aca62f41885f8
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 03:13:30 +0600

    Add files via upload

---
+++ q21.java
@@ -0,0 +1,19 @@
+//write a program to print multiple table of 10  reverse order
+public class q21 {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=10;i>0;i--){
+            System.out.printf("%d X %d=%d\n",n,i,n*i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
