bf7a4bde76d01c9725e1de08ef29b96be6d587ce
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 01:23:41 +0600

    java program for natural numbers from 100 to 200

---
+++ qq2.java
@@ -0,0 +1,13 @@
+//write a java program to print natural numbers from 100 to 200
+public class qq2 {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i <= 200) {
+            System.out.println(i);
+            i++;
+
+        }
+
+    }
+}
+
