41b913f44202af05edb763fd7e8e9db4e25110e3
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-08 02:04:34 +0600

    write a java program to print n odd numbers

---
+++ qq4.java
@@ -0,0 +1,10 @@
+//write a java program to print n odd numbers
+public class qq4 {
+    public static void main(String[] args) {
+        int n=70;
+        for(int i=0;i<n;i++){
+            System.out.println(2*i+1);
+        }
+
+    }
+}
